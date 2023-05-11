01499061545a1edcfb623c8edd4bd2f6a121cd7c
Marie Annonciate Hatungimana <marieannonciate.hatungimana@dci.education>
2023-05-11 10:45:48 +0200

    Adds nav - 3.

--- src/components/PhotoAndVideo.jsx
+++ src/components/PhotoAndVideo.jsx
@@ -1,68 +1,10 @@
 import Footer from "./Footer";
+import Nav from "./Nav";
 
 const PhotoAndVideo = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <a class="navbar-brand" href="/">
-                    <img
-                        className="home-icon"
-                        src="images/home/home-icon.png"
-                        alt="Home"
-                    />
-                </a>
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarTogglerDemo02"
-                    aria-controls="navbarTogglerDemo02"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/office">
-                                Büro
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/smartphone">
-                                Smartphone
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/tv-and-audio">
-                                TV & Audio
-                            </a>
-                        </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="/photo-and-video">
-                                Foto & Video
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/games">
-                                Spiele
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/offers">
-                                Angebote
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/contact">
-                                Kontakt
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+            <Nav pathName={"photo-and-video"} />
             <main>
                 <div class="sub-main rounded-border-element">
                     <div class="articles-container">
--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -1,71 +1,13 @@
 import { useLocation } from "react-router-dom";
 import Footer from "./Footer";
+import Nav from "./Nav";
 
 const ThankYou = () => {
     const location = useLocation();
 
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light">
-                <a className="navbar-brand" href="/">
-                    <img
-                        className="home-icon"
-                        src="images/home/home-icon.png"
-                        alt="Home"
-                    />
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarTogglerDemo02"
-                    aria-controls="navbarTogglerDemo02"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/office">
-                                Büro
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/smartphone">
-                                Smartphone
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/tv-and-audio">
-                                TV & Audio
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/photo-and-video">
-                                Foto & Video
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/games">
-                                Spiele
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/offers">
-                                Angebote
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/contact">
-                                Kontakt
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+            <Nav pathName={"thank-you"} />
             <main>
                 <div className="thank-you-main">
                     <div className="thank-you-text rounded-border-element colored-border-element">
