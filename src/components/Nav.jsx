df2cecf44db6c6028d970656741c40b1fc3ef2eb
Marie Annonciate Hatungimana <marieannonciate.hatungimana@dci.education>
2023-07-10 18:05:58 +0200

    Handles home - 4.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = ({ pathName }) => {
             >
                 <img
                     className="home-icon"
-                    src="images/home/home-icon.png"
+                    src="/public/images/home/home-icon.png"
                     alt="Home"
                 />
             </a>
